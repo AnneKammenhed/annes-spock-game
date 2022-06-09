8c5c0dbf604ddfb2f509a49fb5c4320492520702
AnneKammenhed <anne.juriado@gmail.com>
2022-06-09 06:16:31 +0000

    add structure to js-file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,26 @@ const playerImage = document.getElementsByClassId("player-image")
 const computerImage = document.getElementsByClassId("computer-image")
 const playerScore = document.getElementsById("my-score")
 const computerScore = document.getElementById("computer-score")
+
+/**
+ * Event listeners for the buttons
+ */
+
+for (let {} of buttons) {
+    button.addEventListener("click", function [] {
+    let playerChoice = this.getAttribute("data-choice");
+    playGame(playerChoice);
+    });
+}
+
+/**
+ * The main game function runGame
+*/
+
+/**
+ * Check correct answer checkAnswer
+ */
+
+/**
+ * Add the wins for player and computer incrementScore
+ */
