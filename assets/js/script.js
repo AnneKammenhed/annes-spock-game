f7cbbc89d2a24c43fa0437dbba222361ecb7cc08
AnneKammenhed <anne.juriado@gmail.com>
2022-06-09 05:33:16 +0000

    add constants to js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+/** Constants for DOM elements
+and choices
+*/
+
+const buttons = document.getElementsByClassName("btn");
+const playerImage = document.getElementsByClassId("player-image")
+const computerImage = document.getElementsByClassId("computer-image")
+const playerScore = document.getElementsById("my-score")
+const computerScore = document.getElementById("computer-score")
