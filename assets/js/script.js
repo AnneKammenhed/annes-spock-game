36e6fbd39dd1579de711d7277243fdea2d051c1a
AnneKammenhed <anne.juriado@gmail.com>
2022-06-09 13:01:38 +0000

    add new event listener and rungamefunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,46 @@
 
-/** Constants for DOM elements
-and choices
+/** Constants for DOM elements and choices
 */
 
-const buttons = document.getElementsByTagName("button");
-const playerImage = document.getElementsByClassId("player-image");
-const computerImage = document.getElementsByClassId("computer-image");
-const playerScore = document.getElementsById("my-score");
-const computerScore = document.getElementById("computer-score");
-const choices = ("rock", "paper", "scissors", "lizard", "spock");
 
 /**
  * Event listeners for the buttons to call the main game
  */
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("Button");
 
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-        let playerChoice = this.getAttribute("data-choice"); 
-        playGame(playerChoice);
-    });
-}
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type")) {
+                alert("Let's play Annes Spock Game!");
+            } 
+        })
+    }
+})
 
 /**
- * The main game function playGame, players choise, computer random number and result
+ * The main game function to run the game
+*/
+function runGame() {
+    //choose random computer choice
+    let computerChoice = Math.floor(Math.random() * 4)
+} 
+
+
+/**
+ * Check wich answer is true checkAnswer function
 */
 
-function playGame(playerChoice) {
-    playerImage.src = `assets/images/${[playerChoice]}.jpg`;
-    playerImage.alt = choices[playerChoice];
-
-    let computerChoice = Math.floor(Math.random() * 4);
-
-    computerImage.src = `assets/images/${[computerChoice]}.jpg`;
-    computerImage.alt = choices[computerChoice];
-
-    let result = checkAnswer(choices[playerChoice], choices[computerChoice]);
-
-    updateScores(result);
-}
-
 /**
- * Check answer function checkAnswer
- * 
- */
-function checkAnswer (playerChoise, computerChoice) {
-
-    if(playerChoice === computerChoice) {
-        result = null;
-        alert("It's a draw!");
-        
-    } else if (playerChoice === "rock" && computerChoice === "scissors", "lizard") {
-        result = playerChoice;
-        return `You win`; 
-    } else if (playerChoice === "paper" && computerChoice === "rock", "spock") {
-        result = playerChoice;
-        return `You win`;
-    } else if (playerChoice === "scissors" && computerChoice === "lizard", "paper") {
-        result = playerChoice;
-        return `You win`;
-    } else if (playerChoice === "lizard" && computerChoice === "paper", "spock") {
-        result = playerChoice;
-        return `You win`;
-    } else (playerChoice === "spock" && computerChoice === "scissors", "rock"); {
-        result = playerChoice;
-        return `You win`;
-    }
-}
-
-/**
- * add you win / you loose message - alert? null = draw compoterChoice= you lose player choice - you win
+ * Add the wins for player and computer incrementScore for spans "my-score" and "computer-score"
  */
 
 
 /** 
  * display player choice
  */
-function displayPlayerChoice() {
 
-    if (playerChoice) {
-        displayPlayerImage;
-    }
-}
 
 /**
  * display computer choice
  */
-
- function displayComputerChoice() {
-
-    if (computerChoice) {
-        displayComputerImage;
-    } 
- }
- 
- /**
- * Add the wins for player and computer incrementScore for spans "my-score" and "computer-score"
- */
-
-function incrementPlayerScore() {
-    let oldScore = parseInt(document.getElementById("my-score").innerText);
-    document.getElementById("my-score").innerText = ++oldScore;
-}
-
-function incrementComputerScore() {
-    let oldScore = parseInt(document.getElementById("computer-score").innerText);
-    document.getElementById("computer-score").innerText = ++oldScore;
-}
