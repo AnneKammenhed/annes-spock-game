e1877fc464eeec4dd4ecd674b24338ee7f126d34
AnneKammenhed <anne.juriado@gmail.com>
2022-06-10 06:36:24 +0000

    add js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 
 /** Constants for DOM elements and choices
 */
+const buttons = document.getElementsByTagName("button");
 
+const playerImage = document.getElementsByClassId("player-image");
+const playerScore = document.getElementsById("my-score");
+
+const computerImage = document.getElementsByClassId("computer-image");
+const computerScore = document.getElementById("computer-score");
+
+const choices = ("rock", "paper", "scissors", "lizard", "spock");
 
 /**
- * Event listeners for the buttons to call the main game
+ * Start playing game with the buttons - call the main game
  */
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("Button");
@@ -23,25 +31,58 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * The main game function to run the game
 */
-function runGame(gameType) {
+function runGame(_gameType) {
+
+    playerImage.src = `assets/images/${[playerChoice]}.jpg`;
 
     //choose random computer choice
     let computerChoice = Math.floor(Math.random() * 4);
 
-    if (gameType === "submit") {
-        displaySubmitQuestion(computerChoice);
-        displaySubmitQuestion(playerChoice);
-    }
+    computerImage.src = `assets/images/${[computerChoice]}.jpg`;
+
+    let result = checkAnswer(choices[playerChoice], choices[computerChoice]);
+
+    updateScores(result);
 } 
 
 /**
  * Check wich answer is true checkAnswer function
 */
 
+function checkAnswer (_playerChoise, computerChoice) {
+
+    if(playerChoice === computerChoice) {
+        result.textContent = "It's a draw!";
+        
+    } else if (playerChoice === "rock" && computerChoice === "scissors", "lizard") {
+        result.textContent = "You win!";
+        playerScore++;
+        playerScore.textContent = playerScore; 
+    } else if (playerChoice === "paper" && computerChoice === "rock", "spock") {
+        result = playerChoice;
+        return `You win`;
+    } else if (playerChoice === "scissors" && computerChoice === "lizard", "paper") {
+        result = playerChoice;
+        return `You win`;
+    } else if (playerChoice === "lizard" && computerChoice === "paper", "spock") {
+        result = playerChoice;
+        return `You win`;
+    } else (playerChoice === "spock" && computerChoice === "scissors", "rock"); {
+        result = playerChoice;
+        return `You win`;
+    }
+}
+
 /**
  * Add the wins for player and computer incrementScore for spans "my-score" and "computer-score"
  */
+function incrementPlayerScore () {
 
+}
+
+function incrementComputerScore () {
+
+}
 
 /** 
  * display player choice
